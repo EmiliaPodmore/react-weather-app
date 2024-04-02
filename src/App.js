fa302331651551eb8c99b60fde1566b0d1e20389
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-02 16:49:04 +0100

    Added footer with link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Emilia Podmore, it is{" "}
+        <a
+          href="https://github.com/EmiliaPodmore/react-weather"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and{" "}
+        <a href="" target="_blank">
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
