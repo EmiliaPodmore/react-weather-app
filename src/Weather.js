bb9141ce450608887185ca1b1907519af464cf62
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-06 15:46:34 +0100

    Added icon URL and cleaned CSS

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
       city: response.data.name,
-      iconUrl: "",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
