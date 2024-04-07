54c240298b1cf1203fd64a16de7aa4ba68e74cab
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-07 19:32:38 +0100

    Included icon and now able to build on Netlify

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
       city: response.data.name,
-      iconUrl: response.data.weather[0].icon,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
