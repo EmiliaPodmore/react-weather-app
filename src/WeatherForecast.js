0110500d92b670a78eaa864f0ca6a1cee8295623
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-08 16:07:08 +0100

    Displayed forecast layout

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          <div className="forecast-day">Thurs</div>
-          <WeatherIcon code="01d" />
-          12 18
+          <div className="WeatherForecast-day">Thurs</div>
+          <WeatherIcon code="01d" size={40} />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">18˚C</span>
+            <span className="WeatherForecast-temperature-min">12˚C</span>
+          </div>
         </div>
       </div>
     </div>
