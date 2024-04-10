5347021de3f47b350794e3a986bea6d43a9e810c
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-10 18:15:52 +0100

    Edited columns

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,13 +6,13 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+      <h1 className="currentCity">{props.data.city}</h1>
       <div className="row mt-3">
-        <div className="col-6">
-          <p className="date">
-            <FormattedDate date={props.data.date} />
-          </p>
+        <div className="col-sm-5">
           <ul>
+            <li className="date">
+              <FormattedDate date={props.data.date} />
+            </li>
             <li>
               Humidity: <strong>{props.data.humidity}%</strong>
             </li>
@@ -21,11 +21,11 @@ export default function WeatherInfo(props) {
             </li>
           </ul>
         </div>
-        <div className="col-6 text-center">
-          <div className="d-flex">
-            <WeatherIcon code={props.data.icon} size={52} />
-            <WeatherTemperature celsius={props.data.temperature} />
-          </div>
+        <div className="col-sm-2">
+          <WeatherIcon code={props.data.icon} size={70} />
+        </div>
+        <div className="col-sm-5 text-center ">
+          <WeatherTemperature celsius={props.data.temperature} />
           <p className="text-capitalize description">
             {props.data.description}
           </p>
