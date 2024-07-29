f96d2ba66458e685cf36e0610cac05443d2f78d0
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-07-29 22:54:57 +0100

    Made design clearer and user-friendly

--- src/Weather.js
+++ src/Weather.js
@@ -44,7 +44,6 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city..."
                 autoFocus="on"
                 onChange={handleCityChange}
                 className="form-control"
@@ -54,7 +53,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-dark w-100"
               />
             </div>
           </div>
